6ec759ae7442dca5b26bf6db63571fb4a17860d7
albaceo12 <96391457+albaceo12@users.noreply.github.com>
2025-03-29 22:22:56 +0330

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2025-02-24.acacia",
 });
 const placeOrder = async (req, res) => {
-  const frontend_url = `http://localhost:5174`;
+  const frontend_url = `https://food-del-frontend-5i2b.onrender.com/`;
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
